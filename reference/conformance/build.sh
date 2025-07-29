a82a5e9fb4f71cc7619c0c7738d03b342b9b5bee
Seth Lawler <slawler@dewberry.com>
2025-07-29 10:51:37 -0400

    add conformance

--- reference/conformance/build.sh
+++ reference/conformance/build.sh
@@ -2,12 +2,7 @@ IMAGE=conformance
 
 docker build -t $IMAGE .
 
-# docker run --rm -v ./data:/data $IMAGE "$(cat example-ras-config.json)"
+# docker run --rm --env-file .env \
+#     $IMAGE "$(cat example-conformance-config.json)"
 
-docker run --rm \
-  --env-file .env \
-  $IMAGE \
-  "$(cat example-ras-config.json)"
-
-# docker run --rm -v ./data:/data $IMAGE "$(cat example-ras-config.json)"
-# docker run --rm -v ./data:/data --entrypoint /bin/bash $IMAGE -c "ls -l /data"
+docker run --rm --env-file .env  $IMAGE "$(cat example-conformance-config.json)"
