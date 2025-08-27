06fe2d2ef162e2aa2d053bfe2a1018e586b1121c
Seth Lawler <slawler@dewberry.com>
2025-08-27 12:35:28 -0400

    update config and add error handling to rasUnsteady

--- reference/ras/v660/build.sh
+++ reference/ras/v660/build.sh
@@ -1,7 +1,5 @@
-IMAGE=ffrd_child
+IMAGE=ras
 docker build --platform linux/amd64 -t $IMAGE .
-
-# docker run --rm $IMAGE
 
 docker run --platform linux/amd64  --rm -v ./data:/data $IMAGE "$(cat example-ras-config.json)"
 # docker run --platform linux/amd64 --rm -v ./data:/data --entrypoint /bin/bash $IMAGE -c "ls -l /data"
