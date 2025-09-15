3bfbe7e9c1362d6128bc676e20aa19297edc9653
lalekost <thomas.lalekos@urs.com>
2025-09-15 10:46:31 -0400

    Add HecRas V641

--- reference/ras/v660/v610/build.sh
+++
@@ -1,7 +0,0 @@
-IMAGE=ffrd_child
-#docker build --platform linux/amd64 -t $IMAGE .
-
-# docker run --rm $IMAGE
-
- docker run --platform linux/amd64  --rm -v ./data:/data $IMAGE "$(cat example-ras-config.json)"
- #docker run --platform linux/amd64 --rm -v ./data:/data --entrypoint /bin/bash $IMAGE -c "ls -l /data"
